ba44b74c13a46cddfa83169c242a4f770dc448e8
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-12 14:00:38 -0600

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Joshua Rosado Olivencia on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationTests/NavigationTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationTests.swift
+//  NavigationTests
+//
+//  Created by Joshua Rosado Olivencia on 12/12/24.
+//
+
+import Testing
+@testable import Navigation
+
+struct NavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
