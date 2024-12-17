25dc6dd9404966c382b5b769b7c00af8dcf8c9b2
JoshuaRosado <joshua06rosado@gmail.com>
2024-12-17 15:40:54 -0600

    programmatic navigation

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -7,34 +7,35 @@
 
 import SwiftUI
 
-// Hashing
-// is a computer science term that is the process of converting some data into a smaller representation in a consistent way
 
-
-struct Student: Hashable {
-    var id = UUID()
-    var name: String
-    var age: Int
-}
 
 struct ContentView: View {
+    // Using a path Destination
+    @State private var path = [Int]()
+    
     var body: some View {
-        NavigationStack{
-            List(0..<100) { i in
-                // title - i, value itself - i
-                NavigationLink("Select \(i)", value: i)
+        // Selecting path = $path
+        NavigationStack(path: $path){
+            VStack{
+                Button("Show 32") { // Tap
+                    path = [32] // Show 32
+                }
+                
+                Button("Show 64"){
+                    path.append(64) // Add 64 to the Path and Display it
+                }
+                
+                Button("Show 32 then 64"){
+                    path = [32,64] // Display 2 destinations in path
+                }
+                
             }
+            // Destionation for Integer itself
+            .navigationDestination(for: Int.self)
+                { selection in // passing in a selection
+                Text("You selected \(selection)")}
             
-            // navigationLink selected will display itself
-            // if Select 2 is taped, "You selected 2" will be displayed
-            .navigationDestination(for: Int.self) { selection in
-                Text( "You selected \(selection)")
-            }
-            .navigationDestination(for: Student.self) { student in
-                Text( "You selected \(student)")
-            }
         }
-        
     }
 }
 
